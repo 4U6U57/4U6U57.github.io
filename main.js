2bbedc7d95ad87800500f7c55d5f911be7e4697a
August Valera <4U6U57@gmail.com>
2015-03-08 17:55:56 -0700

    Separate pages work now.

--- main.js
+++ main.js
@@ -2,3 +2,18 @@ var toastGroupTemplate = document.querySelector('#toastGroup');
 toastGroupTemplate.showToast = function() {
   document.querySelector('#toast').show();
 }
+
+function pageZero(){
+  var c = document.querySelector('core-animated-pages');
+  c.selected = 0;
+}
+
+function pageOne(){
+  var c = document.querySelector('core-animated-pages');
+  c.selected = 1;
+}
+
+function pageTwo(){
+  var c = document.querySelector('core-animated-pages');
+  c.selected = 2;
+}
