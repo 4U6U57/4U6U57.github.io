61514c17ff1425f1ad861bbd4a6566bdcc7d8376
August Valera <4U6U57@gmail.com>
2015-03-25 23:07:28 -0700

    Simplified main.js after learning Javascript on Codeacademy.

--- main.js
+++ main.js
@@ -3,17 +3,6 @@ toastGroupTemplate.showToast = function() {
   document.querySelector('#toast').show();
 }
 
-function pageZero(){
-  var c = document.querySelector('core-animated-pages');
-  c.selected = 0;
-}
-
-function pageOne(){
-  var c = document.querySelector('core-animated-pages');
-  c.selected = 1;
-}
-
-function pageTwo(){
-  var c = document.querySelector('core-animated-pages');
-  c.selected = 2;
+function turnPage(page) {
+  document.querySelector('core-animated-pages').selected = page;
 }
