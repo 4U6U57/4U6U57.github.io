55df9f2f4b268395129178b05d5f23b5b5d3fa02
August Valera <4U6U57@gmail.com>
2015-03-27 22:31:07 -0700

    Polish up tabs, disable drawer entries.

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var toastGroupTemplate = document.querySelector('#toastGroup');
 toastGroupTemplate.showToast = function() {
   document.querySelector('#toast').show();
-}
+};
 
 function turnPage(page) {
   document.querySelector('core-animated-pages').selected = page;
+  document.querySelector('core-drawer');
+}
+function toaster(id) {
+  document.querySelector(id).show();
 }
