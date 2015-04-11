c88cf43087137f39314b4b20002c8568362aa2dd
August <4U6U57@gmail.com>
2015-04-11 02:08:16 -0700

    Added enhancements to 404 page

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-
 function toaster(id) {
   document.querySelector(id).show();
 }
 function dialogue(id) {
   document.querySelector(id).toggle();
+}
+function link(url) {
+  window.location.href = url;
 }
